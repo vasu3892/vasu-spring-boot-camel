7f08badf6bd932df508e9550b61464e41b2e7eec
Vasudev <vasu3892@gmail.com>
2020-09-09 12:55:10 +0530

    exposed a rest GET endpoint

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -15,27 +15,25 @@
  */
 package io.fabric8.quickstarts.camel;
 
-import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
-/**
- * A spring-boot application that includes a Camel route builder to setup the Camel routes
- */
 @SpringBootApplication
-@ImportResource({"classpath:spring/camel-context.xml"})
-public class Application extends RouteBuilder {
+public class Application {
 
-    // must have a main method spring-boot can run
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	// must have a main method spring-boot can run
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Override
-    public void configure() throws Exception {
-        from("timer://foo?period=5000")
-            .setBody().constant("Hello World")
-            .log(">>> ${body}");
-    }
+	@Bean
+	public ServletRegistrationBean camelServletRegistrationBean() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/camel/*");
+		registration.setName("CamelServlet");
+		return registration;
+	}
+
 }
